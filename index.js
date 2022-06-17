8891876c8819d16893caf944d33403db1d1aa3d3
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-17 23:41:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+    // Write your algorithm here
+    // I expect calling getNumbers with an array [5, "11", 7] will return [5, 7]
+    console.log("Expecting : [5, 7]");
+    console.log("=>", getNumbers([5, "11", 7]));
+
+    // I expect calling getNumbers with an array [5, 7] will return [5, 7]
+    console.log("Expecting : [5 ,7]");
+    console.log("=>, getNumbers([5, 7])");
 }
 
 /* 
   Add your pseudocode here
-*/
+  initiate empty array called results
 
-/*
-  Add written explanation of your solution here
+  iterate over each item in the input array:
+     if element is a number:
+     push item onto result
+
+  return result
 */
+If the string is a Palindrome
+
+print "true"
+
+else
+
+    print "false"
+    /*
+      Add written explanation of your solution here
+
+    */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+    // add your own custom tests in here
+    console.log("Expecting: true");
+    console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+    console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+    console.log("Expecting: false");
+    console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
